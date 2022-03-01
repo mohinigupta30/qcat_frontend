56254c46b3442b9cb1216ccfbb902d7a0207a139
Mohini Gupta <mohinigupta@dhcp-10-105-217-219.wireless.northwestern.private>
2022-03-01 11:09:21 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask import render_template
+import random
+import json
+from helpers import CurrentUser
+from pprint import pprint
+
+# initializes flask app:
+app = Flask(__name__)
+
+#########################
+# some global variables #
+#########################
+current_category = ""
+##############
+# Exercise 1 #
+##############
+@app.route('/')
+def home():
+    return render_template(
+        'q-cat-home-page.html',
+        category = current_category
+    )
+
+@app.route('/question', methods=['POST'])
+def categorize():
+    question = request.form['entered_question']
+    return render_template(
+        'q-cat-home-page.html',
+        category = question
+    )
